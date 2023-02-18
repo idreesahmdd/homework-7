f17d30c2c4fd3d46c120edf977c1708ddc6e87f2
idreesahmdd <you@example.com>
2023-02-18 20:02:14 +0700

    coba async

--- soal2.js
+++ soal2.js
@@ -1,7 +1,27 @@
 const fs = require("fs");
 
+// ---------SYNCHRONOUS------------
 const data = fs.readFileSync("./homework.txt", "utf-8");
 // console.log(data);
 
 fs.writeFileSync("log.txt", data);
-// console.log("BERHASIL DITULIS");
+console.log("BERHASIL DITULIS");
+
+// ===================================================================================
+
+//  ---------ASYNCHRONOUS-----------
+// fs.readFile("./homework.txt", "utf-8", (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+
+//     console.log(data);
+
+//     fs.writeFile("log.txt", data, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         }
+
+//         console.log("TULIS BERHASIL");
+//     });
+// });
